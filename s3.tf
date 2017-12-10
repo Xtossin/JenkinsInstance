2bfe82a14c4f27ea88279a08059a4efd547e3a47
xtossin <christospavlatos@gmail.com>
2017-12-10 14:17:35 -0800

    added comment to s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,6 @@
 
-#this will output/cope my terraform state from my local computer into an S3 bucket.
+/*this will create an S3 to output/copy my terraform state from my local computer into an S3 bucket.
+See configure-remote-state.*/
 resource "aws_s3_bucket" "terraform-state" {
     bucket = "terraform-state-qwerty123456787q"
     acl = "private"
